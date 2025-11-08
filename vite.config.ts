4f154b71593c9dd70767890031fae967f90cc8fc
JAIPRAKASH <sjaiprakash682@gmail.com>
2025-11-08 19:49:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 Must match your GitHub repo name
+  base: "/newvoltai/", // 👈 Must match your GitHub repo name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // 👈 Enables imports like "@/components/..."
